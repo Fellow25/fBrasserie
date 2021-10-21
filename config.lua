39e4dba96e72da6943dc75439ab1bace36cf6ec7
Fellow25 <92899080+Fellow25@users.noreply.github.com>
2021-10-21 02:24:08 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,64 @@
+brasserie             = {}
+
+
+
+brasserie.jeveuxmarker = true --- true = Oui | false = Non
+
+brasserie.jeveuxblips = true --- true = Oui | false = Non
+
+brasserie.argentjoueur = 5 --- prix de la prime de vente
+
+brasserie.ventemin = 100 --- vente minimum
+brasserie.ventemax = 200 --- vente maximale
+
+brasserie.pos = {
+	coffre = {
+		position = {x = -513.35, y = -34.73, z = 45.72}
+	},
+	garage = {
+		position = {x = -524.8, y = -37.68, z = 44.52}
+	},
+	spawnvoiture = {
+		position = {x = -524.8, y = -37.68, z = 44.52, h = 358.05}
+	},
+	boss = {
+		position = {x = -504.96, y = -21.47, z = 45.73}
+	},
+	blips = {
+		position = {x = -509.43, y = -21.39, z = 45.61}
+	},
+	recolte = {
+		position = {x = 323.137, y = 6483.598, z = 29.13}
+	},
+	transformation = {
+		position = {x = -689.382, y = 5788.966, z = 17.33}
+	},
+	vente = {
+		position = {x = -288.173, y = 6299.457, z = 31.49}
+	},
+}
+
+Gbrasserievoiture = {
+	{nom = "Rat Loader", modele = "ratloader2"},
+}
+
+
+brasserie.Uniforms = {
+	male = {
+		['bags_1'] = 0, ['bags_2'] = 0,
+		['tshirt_1'] = 15, ['tshirt_2'] = 0,
+		['torso_1'] = 41, ['torso_2'] = 0,
+		['arms'] = 1,
+		['pants_1'] = 7, ['pants_2'] = 0,
+		['shoes_1'] = 12, ['shoes_2'] = 0,
+},
+
+female = {
+	['bags_1'] = 0, ['bags_2'] = 0,
+	['tshirt_1'] = 23,['tshirt_2'] = 4,
+	['torso_1'] = 52, ['torso_2'] = 0,
+	['arms'] = 5, ['arms_2'] = 0,
+	['pants_1'] = 36, ['pants_2'] = 0,
+	['shoes_1'] = 42, ['shoes_2'] = 1,
+}
+}
